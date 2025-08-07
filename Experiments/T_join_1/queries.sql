13bd66fa578b869f502cbe4dfa0bfcdfbda55cfd
DarthPiotr <vader.piotr@gmail.com>
2025-08-07 23:42:07 +0200

    Fixes

--- Experiments/T_join_1/queries.sql
+++ Experiments/T_join_1/queries.sql
@@ -73,14 +73,18 @@ WHERE
 
 ROLLBACK;
 
-"Gather  (cost=5411.77..45287.44 rows=58600 width=266)"
+"Gather Merge  (cost=43757.60..49455.30 rows=48834 width=266)"
 "  Workers Planned: 2"
-"  ->  Parallel Hash Join  (cost=4411.77..38427.44 rows=24417 width=266)"
-"        Hash Cond: (o.o_custkey = c.c_custkey)"
-"        ->  Parallel Seq Scan on orders o  (cost=0.00..32375.00 rows=625000 width=107)"
-"        ->  Parallel Hash  (cost=4381.25..4381.25 rows=2442 width=159)"
-"              ->  Parallel Seq Scan on customer c  (cost=0.00..4381.25 rows=2442 width=159)"
-"                    Filter: (c_nationkey = 10)"
+"  ->  Sort  (cost=42757.58..42818.62 rows=24417 width=266)"
+"        Sort Key: orders.o_custkey"
+"        ->  Parallel Hash Join  (cost=3871.95..37887.62 rows=24417 width=266)"
+"              Hash Cond: (orders.o_custkey = customer.c_custkey)"
+"              ->  Parallel Seq Scan on orders  (cost=0.00..32375.00 rows=625000 width=107)"
+"              ->  Parallel Hash  (cost=3841.42..3841.42 rows=2442 width=159)"
+"                    ->  Parallel Bitmap Heap Scan on customer  (cost=69.71..3841.42 rows=2442 width=159)"
+"                          Recheck Cond: (c_nationkey = 10)"
+"                          ->  Bitmap Index Scan on idx_customer_nationkey  (cost=0.00..68.24 rows=5860 width=0)"
+"                                Index Cond: (c_nationkey = 10)"
 
 -- Result 
 
